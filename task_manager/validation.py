50b30422bf275c7076ec70cb606821d2907df1fd
kioi23 <kioijgithinji23@gmail.com>
2026-06-07 22:54:38 +0300

    Implement validation functions for task title, description, and due date

--- task_manager/validation.py
+++ task_manager/validation.py
@@ -1,10 +1,37 @@
 from datetime import datetime
 
 def validate_task_title(title):
-    None
-    
+    """
+    Validates task title.
+    Returns True if valid, False otherwise.
+    """
+
+    if len(title.strip()) == 0:
+        print("Error: Title cannot be empty.")
+        return False
+
+    return True
+
+
 def validate_task_description(description):
-    None    
-    
+    """
+    Validates task description.
+    """
+
+    if len(description.strip()) == 0:
+        print("Error: Description cannot be empty.")
+        return False
+
+    return True
+
+
 def validate_due_date(due_date):
-    None
+    """
+    Validates due date.
+    """
+
+    if len(due_date.strip()) == 0:
+        print("Error: Due date cannot be empty.")
+        return False
+
+    return True
